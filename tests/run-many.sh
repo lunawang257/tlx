dcfdc7fd8d552c1a1b69c8b34c729880763c5be6
Luna Wang <lunawang257@gmail.com>
2024-09-17 22:46:22 -0700

    update args in run-many.sh

--- tests/run-many.sh
+++ tests/run-many.sh
@@ -13,27 +13,23 @@ REPEAT=64
 MAX_THREAD=6
 N=1024000
 
-prog="$SCRIPT_DIR/../build/tests/tlx_container_btree_speedtest_concurrent"
+prog="$SCRIPT_DIR/../build/Release/tests/tlx_container_btree_speedtest_concurrent"
 
 rm -f /tmp/out
 
-for rootSlot in 1 -2; do
-    for lockReq in all root no-root none ; do
-	for slotMax in 4 64 256 ; do
-	    for ((thread=1;thread<=$MAX_THREAD;thread++)); do
-		printf '%02d:%02d: ' "$(( SECONDS/60 ))" "$(( SECONDS%60 ))"
-		echo "$prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq > /tmp/one-out"
-		if [ "$dryrun" != "1" ] ; then
-		    $prog -r 64 -m $N -M $N -i 0 -l 100 -s -t $thread -R $rootSlot -L $lockReq > /tmp/one-out
-		    if [ ! -f "/tmp/out" ]; then
-			tail -2 /tmp/one-out
-			tail -2 /tmp/one-out > /tmp/out
-		    else
-			tail -1 /tmp/one-out
-			tail -1 /tmp/one-out >> /tmp/out
-		    fi
-		fi
-	    done
-	done
-    done
+for slotMax in 64 256 ; do
+   for ((thread=1;thread<=$MAX_THREAD;thread++)); do
+      printf '%02d:%02d: ' "$(( SECONDS/60 ))" "$(( SECONDS%60 ))"
+      echo "$prog -r 64 -m $N -M $N -i 0 -l 100 -t $thread > /tmp/one-out"
+      if [ "$dryrun" != "1" ] ; then
+         $prog -r 64 -m $N -M $N -i 0 -l 100 -t $thread > /tmp/one-out
+         if [ ! -f "/tmp/out" ]; then
+            tail -2 /tmp/one-out
+            tail -2 /tmp/one-out > /tmp/out
+         else
+            tail -1 /tmp/one-out
+            tail -1 /tmp/one-out >> /tmp/out
+         fi
+      fi
+   done
 done
