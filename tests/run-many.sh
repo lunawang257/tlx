afb177f837d078e398f09899da599a9419f43768
Luna Wang <lunawang257@gmail.com>
2024-09-20 07:04:55 -0700

    use gprof to profile after each run.

--- tests/run-many.sh
+++ tests/run-many.sh
@@ -17,44 +17,62 @@ if [ "$1" != "" ]; then
 fi
 
 # smaller will reduce run time
-REPEAT=64
+REPEAT=16
 MAX_THREAD=6
 N=1024000
+sliceSize=32
+sliceSizeMax=64
+insertProp=34
+lookupProp=33
 
 prog="$SCRIPT_DIR/../build/Release/tests/tlx_container_btree_speedtest_controller"
 
-rm -f $out
+rm -f "$out"
 
+# shellcheck disable=SC2043
 for slotMax in 512 ; do
+    # shellcheck disable=SC2043
     for valSize in 512 ; do
+        # shellcheck disable=SC2043
         for maplize_threshold in 0 100 ; do
+            # shellcheck disable=SC2043
             for dist in zipf uniform ; do
-                for ((thread=1;thread<=$MAX_THREAD;thread++)); do
+                for ((thread=1;thread<=MAX_THREAD;thread++)); do
                     printf '%02d:%02d: ' "$(( SECONDS/60 ))" "$(( SECONDS%60 ))"
+                    runName="SlotMax-$slotMax-ValSize-$valSize-SliceSz-$sliceSize"
+                    runName="${runName}-SlcSzMx-$sliceSizeMax-Thread-$thread"
+                    runName="${runName}-MplThrh-$maplize_threshold-Dist-$dist"
+                    runName="${runName}-InsertP-$insertProp-LookupP-$lookupProp"
+                    echo $runName
                     cmd="$prog \
 --test btreemix \
 --slot-max $slotMax \
 --val-size $valSize \
 --iteration $N \
 --num-threads $thread \
---slice-size 32 \
---slice-size-max 64 \
+--slice-size $sliceSize \
+--slice-size-max $sliceSizeMax \
 --maplize-threshhold $maplize_threshold \
--I 34 \
--L 33 \
+-I $insertProp \
+-L $lookupProp \
 --dist $dist \
 --repeats $REPEAT
 > /tmp/one-out"
                     echo "$cmd"
                     if [ "$dryrun" != "1" ] ; then
-                        eval $cmd
+                        eval "$cmd"
                         if [ ! -f "$out" ]; then
                             tail -2 /tmp/one-out
-                            tail -2 /tmp/one-out > $out
+                            tail -2 /tmp/one-out > "$out"
                         else
                             tail -1 /tmp/one-out
-                            tail -1 /tmp/one-out >> $out
+                            tail -1 /tmp/one-out >> "$out"
                         fi
+                    fi
+                    # generate perf profile on Linux
+                    if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+                        gprof "$prog" gmon.out > "$out-gmon-$runName.txt"
+                        rm gmon.out
                     fi
                 done
             done
