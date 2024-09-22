a17d07d124d80694820090ffb2220f0cff5a7773
Luna Wang <lunawang257@gmail.com>
2024-09-22 08:16:52 -0700

    build param of leaf and btreemix tests separately to reduce build time

--- tests/container/gen-all-options.py
+++ tests/container/gen-all-options.py
@@ -11,33 +11,33 @@ script_dir = os.path.dirname(__file__)
 
 # set to run to and re-run to reduce compile time
 # please set to False before checkin
-fast_compile = False
+btreemix_fast_compile = True
+leaf_fast_compile = True
 
-slot_max_list = [32, 128, 256, 512]
-value_size_list = [64, 128, 256, 512]
+btreemix_slot_max_list = [32, 128, 256, 512]
+btreemix_value_size_list = [64, 128, 256, 512]
+
+leaf_slot_max_list = [32, 64, 128, 256, 512]
+leaf_value_size_list = [32, 64, 128, 256, 512]
+
+if btreemix_fast_compile:
+    btreemix_slot_max_list = [32, 512]
+    btreemix_value_size_list = [32, 512]
+
+if leaf_fast_compile:
+    leaf_slot_max_list = [64]
+    leaf_value_size_list = [128]
 
 # List of command formats to be included in the single file
 command_formats = [
+    'RUN_BTREEMIX({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n', # btreemix test
+    # all rest belong to leaf tests
     'RUN_MAPLIZE({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
     'RUN_UPDATE({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
     'RUN_LOOKUP({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
     'RUN_SCAN({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
-    'RUN_BTREEMIX({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
     'RUN_REBALANCE({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
 ]
-
-if fast_compile:
-    slot_max_list = [32, 512]
-    value_size_list = [32, 512]
-    # only leave the command you want to measure
-    command_formats = [
-        #'RUN_MAPLIZE({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
-        #'RUN_UPDATE({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
-        #'RUN_LOOKUP({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
-        #'RUN_SCAN({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
-        #'RUN_BTREEMIX({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n'
-        'RUN_REBALANCE({slot_max}, {value_size}, {slice_size}, {slice_size_max});\n',
-    ]
 
 def update_file_if_different(old_file, new_file):
     """
@@ -100,47 +100,57 @@ def extract_name(input_str):
     else:
         return None
 
-new_names = []
+def main():
+    new_names = []
 
-total_lines = 0
-for command_format in command_formats:
-    sub_name = extract_name(command_format)
-    old_name = f'{script_dir}/btree_speedtest_{sub_name}_options.hpp'
-    new_name = old_name + ".new"
-    new_names.append((old_name, new_name))
-    with open(new_name, 'w') as f:
-        lines = 0
-        f.write('''// DO NOT EDIT! please change gen-leaf-cmd.py to edit
-void run_all_args() {
-''')
-        sub_name_upper = sub_name.upper()
-        f.write(f'    testOptions.insert({sub_name_upper});\n')
-        for slot_max in slot_max_list:
-            for value_size in value_size_list:
-                max_slice_size_exp = int(math.log2(slot_max// 2))
-                slice_size_list = [2 ** i for i in range(3, max_slice_size_exp + 1)]
-                for slice_size in slice_size_list:
-                    if slice_size < 8: # minimum slice size is 8
-                        continue
-                    if slice_size * 4 > slot_max: # minimum num slices is 4
-                        continue
-                    for slice_size_max in [slice_size + 1, int(slice_size * 1.5), slice_size * 2]: #, slice_size * 3]:
-                        if slice_size_max > slot_max: break
-                        line = command_format.format(slot_max=slot_max,
-                                                     value_size=value_size,
-                                                     slice_size=slice_size,
-                                                     slice_size_max = slice_size_max)
-                        f.write('    ') # indentation
-                        f.write(line)
-                        lines += 1
-                        total_lines += 1
-        f.write('}\n')
+    total_lines = 0
+    for command_format in command_formats:
+        sub_name = extract_name(command_format)
+        old_name = f'{script_dir}/btree_speedtest_{sub_name}_options.hpp'
+        new_name = old_name + ".new"
+        new_names.append((old_name, new_name))
+        with open(new_name, 'w') as f:
+            lines = 0
+            f.write('''// DO NOT EDIT! please change gen-leaf-cmd.py to edit
+    void run_all_args() {
+    ''')
+            sub_name_upper = sub_name.upper()
+            f.write(f'    testOptions.insert({sub_name_upper});\n')
+            if sub_name == "btreemix":
+                slot_max_list = btreemix_slot_max_list
+                value_size_list = btreemix_value_size_list
+            else:
+                slot_max_list = leaf_slot_max_list
+                value_size_list = leaf_value_size_list
+            for slot_max in slot_max_list:
+                for value_size in value_size_list:
+                    max_slice_size_exp = int(math.log2(slot_max// 2))
+                    slice_size_list = [2 ** i for i in range(3, max_slice_size_exp + 1)]
+                    for slice_size in slice_size_list:
+                        if slice_size < 8: # minimum slice size is 8
+                            continue
+                        if slice_size * 4 > slot_max: # minimum num slices is 4
+                            continue
+                        for slice_size_max in [slice_size + 1, int(slice_size * 1.5), slice_size * 2]: #, slice_size * 3]:
+                            if slice_size_max > slot_max: break
+                            line = command_format.format(slot_max=slot_max,
+                                                         value_size=value_size,
+                                                         slice_size=slice_size,
+                                                         slice_size_max = slice_size_max)
+                            f.write('    ') # indentation
+                            f.write(line)
+                            lines += 1
+                            total_lines += 1
+            f.write('}\n')
 
-# update old file only if it has changed so Makefile doesn't build unchanged ones
-for name_pair in new_names:
-    old_name = name_pair[0]
-    new_name = name_pair[1]
-    update_file_if_different(old_name, new_name)
-    os.remove(new_name)
+    # update old file only if it has changed so Makefile doesn't build unchanged ones
+    for name_pair in new_names:
+        old_name = name_pair[0]
+        new_name = name_pair[1]
+        update_file_if_different(old_name, new_name)
+        os.remove(new_name)
 
-print(f"Generated {total_lines} lines in {len(command_formats)} files, {lines} per file")
+    print(f"Generated {total_lines} lines in {len(command_formats)} files")
+
+if __name__ == "__main__":
+    main()
