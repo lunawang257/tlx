c1868dc6c0b7e63cfd771e38ecc2e867c0640561
Luna Wang <lunawang257@gmail.com>
2024-10-02 01:06:01 -0700

    do not need leaf perf test yet

--- tests/container/gen-all-options.py
+++ tests/container/gen-all-options.py
@@ -25,8 +25,8 @@ if btreemix_fast_compile:
     btreemix_value_size_list = [256]
 
 if leaf_fast_compile:
-    leaf_slot_max_list = [512]
-    leaf_value_size_list = [512]
+    leaf_slot_max_list = []
+    leaf_value_size_list = []
 
 # List of command formats to be included in the single file
 command_formats = [
